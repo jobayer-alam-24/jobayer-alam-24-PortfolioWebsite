9728e0e750f66046b94e2e46d34119a32a8568b2
jobayer-alam-24 <sheikhnayamul123@gmail.com>
2024-04-01 22:32:27 -0700

    Portfolio website

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,8 +15,11 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            sec.classList.add('show-animate');
         }
-
+        else{
+            sec.classList.remove('show-animate');
+        }
     });
 
     let header = document.querySelector('header');
